7d5743123efd13b760d219d5c02e38661c9642dc
Mateusz Spalek <spalek.mat@gmail.com>
2019-02-20 11:05:03 +0100

    Repair crash with login

--- src/app/panel/auth.service.ts
+++ src/app/panel/auth.service.ts
@@ -7,64 +7,53 @@ import { CarService } from '../car.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService implements OnInit {
+export class AuthService {
 
   user: User;
-  adminToken;
+  adminId = 'LANL5b3YmSdZW9I3gIdYNUkI7e43';
   public showEmailError;
   public showPassError;
 
   constructor(public angularFire: AngularFireAuth, private router: Router, private carService: CarService) {
     angularFire.authState.subscribe(user => {
       this.user = user;
-
-      });
-  }
-  ngOnInit() {
-
+    });
   }
 
 
   login(email: string, password: string) {
     this.angularFire.auth.signInWithEmailAndPassword(email, password)
-    .then(user => {
-      console.log(user);
-      if ( user.user.uid === 'LANL5b3YmSdZW9I3gIdYNUkI7e43') {
-        this.adminToken = true;
-        this.router.navigate(['/admin/orders']);
-      } else {
-        this.adminToken = false;
-        this.router.navigate(['/reserve']);
-      }
-      this.carService.checkOutCar = null;
-    })
-    .catch(err => {
-      this.showEmailError = JSON.parse(JSON.stringify(err.message));
-      console.log(this.showEmailError);
-    });
+      .then(user => {
+        if (user.user.uid === this.adminId) {
+          this.router.navigate(['/admin/orders']);
+        } else {
+          this.router.navigate(['/reserve']);
+        }
+        this.carService.checkOutCar = null;
+      })
+      .catch(err => {
+        this.showEmailError = JSON.parse(JSON.stringify(err.message));
+        console.log(this.showEmailError);
+      });
   }
 
   signup(email: string, password: string) {
     this.angularFire.auth.createUserWithEmailAndPassword(email, password)
-    .then(user => {
-      if ( user.user.uid === 'LANL5b3YmSdZW9I3gIdYNUkI7e43') {
-        this.adminToken = true;
-        this.router.navigate(['/admin/orders']);
-      } else {
-        this.adminToken = false;
-        this.router.navigate(['/reserve']);
-      }
-      this.carService.checkOutCar = null;
-
-    })
-    .catch(err => {
-      this.showPassError = JSON.parse(JSON.stringify(err.message));
-      console.log(this.showPassError);
-    });
+      .then(user => {
+        if (user.user.uid === this.adminId) {
+          this.router.navigate(['/admin/orders']);
+        } else {
+          this.router.navigate(['/reserve']);
+        }
+        this.carService.checkOutCar = null;
+      })
+      .catch(err => {
+        this.showPassError = JSON.parse(JSON.stringify(err.message));
+        console.log(this.showPassError);
+      });
   }
   logout() {
     this.angularFire.auth.signOut();
-    this.showEmailError = null ;
-    this.adminToken = false;
+    this.showEmailError = null;
   }
-  }
+}
